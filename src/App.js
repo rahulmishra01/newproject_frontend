01856433ad4e3288b09168d27a962fad6a0bc6cb
rahulmishra01 <rahulmishra3865@gmail.com>
2024-05-22 11:35:46 +0530

    added the changes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useEffect, useRef, useState } from "react";
 import Peer from "simple-peer";
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000");
+const socket = io("https://new-omagle.onrender.com");
 
 function App() {
   const [me, setMe] = useState("");
